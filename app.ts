c80c40e5201273d8319cdd4595832925e8b6ae07
Arianit <arianit.029@hotmail.com>
2022-06-30 23:06:40 +0200

    setup server

---
+++ app.ts
@@ -0,0 +1,30 @@
+// import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+
+// setup server
+// const server = serve(() => new Response("Hellooo World 2\n"));
+
+// for await(const req of server){
+//     if(req.url === '/'){
+//         req.respond({
+//             status: 200,
+//             body: await Deno.open('./public/index.html')
+//         })
+//     }
+// }
+
+import {Application} from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+const app = new Application();
+
+app.use(async (ctx, next) => {
+    try{
+        await ctx.send({
+            root: `${Deno.cwd()}/public`,
+            index:"index.html",
+        });
+    }catch{
+        await next();
+    }
+});
+
+await app.listen({ port: 8000 });
